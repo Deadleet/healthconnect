93fe3f5555636ef23645e83b3bc20462516500be
thomas guerin <thomasguerin1313@outlook.com>
2023-06-27 11:16:11 +0200

    validations de prescription

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,3 +1,9 @@
 class Prescription < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { minimum: 6 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :practician, presence: true
 end
