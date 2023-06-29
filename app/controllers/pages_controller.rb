3f76ed32e5807668c6545376dd91a862da6a3d5f
clemence dagenais everell <cdagenaiseverell@gmail.com>
2023-06-29 16:45:36 +0200

    view_appointment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,21 +2,21 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    start_date = params.fetch(:start_date, Date.today).to_date
-    end_date = params.fetch(:start_date, Date.today).to_date
-    # # PRESCRIPTIONS
-    @prescriptions = Prescription.where(user: current_user, end_date: end_date.beginning_of_month.beginning_of_week..end_date.end_of_month.end_of_week)
+    # start_date = params.fetch(:start_date, Date.today).to_date
+    # end_date = params.fetch(:start_date, Date.today).to_date
+    # # # PRESCRIPTIONS
+    # @prescriptions = Prescription.where(user: current_user, end_date: end_date.beginning_of_month.beginning_of_week..end_date.end_of_month.end_of_week)
 
-    # # FOLLOw-UPS
-    prescriptions_user = Prescription.where(user_id: current_user)
-    follow_ups = prescriptions_user.flat_map { |prescription| prescription.follow_ups }
-    @follow_ups = FollowUp.where(id: follow_ups.pluck(:id))
-    @follow_ups.where(start_date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+    # # # FOLLOw-UPS
+    # prescriptions_user = Prescription.where(user_id: current_user)
+    # follow_ups = prescriptions_user.flat_map { |prescription| prescription.follow_ups }
+    # @follow_ups = FollowUp.where(id: follow_ups.pluck(:id))
+    # @follow_ups.where(start_date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
 
-    # # MEASURES
-    measures = prescriptions_user.flat_map { |prescription| prescription.measures }
-    @measures = Measure.where(id: measures.pluck(:id))
-    @measures.where(start_date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+    # # # MEASURES
+    # measures = prescriptions_user.flat_map { |prescription| prescription.measures }
+    # @measures = Measure.where(id: measures.pluck(:id))
+    # @measures.where(start_date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
 
   end
 end
