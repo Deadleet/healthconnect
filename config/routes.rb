eccb357a8d74953c7957564aea96dde50a0a07c9
thomas guerin <thomasguerin1313@outlook.com>
2023-06-30 20:57:53 +0200

    maj ok => page historique + auto mesure ok  + prise en compte de la date du jour

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "pages#home"
+  get "historical", to: "pages#historical"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 resources :prescriptions do
   resources :follow_ups, only: %i[index show new create edit update]
