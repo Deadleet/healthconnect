d9be0ab70f54dafde0aecf5575b4bf0fcef62db1
thomas guerin <thomasguerin1313@outlook.com>
2023-07-04 10:53:16 +0200

    maj-graph

--- db/seeds.rb
+++ db/seeds.rb
@@ -63,11 +63,11 @@ puts 'done'
 
 puts '2 creating measurement'
 measurement1 = Measurement.create!(value: "140", unit: "mmHg", measure: measure1, created_at: measure1.start_date)
-measurement2 = Measurement.create!(value: "150", unit: "mmHg", measure: measure1, created_at: measure1.start_date + 40.day)
-measurement3 = Measurement.create!(value: "200", unit: "mmHg", measure: measure1, created_at: measure1.start_date)
-measurement4 = Measurement.create!(value: "1000", unit: "mmHg", measure: measure1, created_at: measure1.start_date + 20.day)
-measurement5 = Measurement.create!(value: "120", unit: "mmHg", measure: measure1, created_at: measure1.start_date)
-measurement6 = Measurement.create!(value: "450", unit: "mmHg", measure: measure1, created_at: measure1.start_date + 30.day)
+measurement2 = Measurement.create!(value: "150", unit: "mmHg", measure: measure1, created_at: measure1.start_date + 1.day)
+measurement3 = Measurement.create!(value: "160", unit: "mmHg", measure: measure1, created_at: measure1.start_date + 2.day)
+measurement4 = Measurement.create!(value: "155", unit: "mmHg", measure: measure1, created_at: measure1.start_date + 3.day)
+measurement5 = Measurement.create!(value: "165", unit: "mmHg", measure: measure1, created_at: measure1.start_date + 4.day)
+measurement6 = Measurement.create!(value: "140", unit: "mmHg", measure: measure1, created_at: measure1.start_date + 5.day)
 puts "2 measurements created!"
 
 appointment1 = Appointment.create!(title: 'Dentiste', appointment_date: Date.new(2023, 03, 11), appointment_time: Time.now, address: "Place de l'ecureuil", user: user1)
